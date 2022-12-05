560ee956402518e7a495546919a55e0b2be2d6e3
FrankAlbella <frankalbella@gmail.com>
2022-12-04 19:29:43 -0500

    Client: Change port number to use peerID

--- src/main/java/cnt/client/Client.java
+++ src/main/java/cnt/client/Client.java
@@ -17,11 +17,13 @@ public class Client {
     boolean hasDownloadStarted;
     ClientState state;
 
-    Handshake handshakeMessage = new Handshake();
     List<Integer> missing = new ArrayList<>();
 
     public Client(String id) {
         log(Config.getString(), id);
+
+        state = new ClientState();
+        state.loadPeerInfo();
 
         //search peer list to find self by id
         for(int i = 0; i < state.getPeers().size(); i++) {
@@ -32,8 +34,7 @@ public class Client {
             }
         }
 
-        state = new ClientState(this.selfInfo);
-        state.loadPeerInfo();
+        state.setSelfInfo(this.selfInfo);
 
         //if the file has already been downloaded (all bits in the bitfield are non-zero)
         if(this.selfInfo.getBitfield()[0] != 0) {
@@ -48,13 +49,16 @@ public class Client {
             for(int i = 0; i < Config.MAX_CONNECT_ATTEMPTS; i++) {
                 try {
                     // TODO change "localhost" to real hostname
-                    Socket socket = new Socket("localhost", Config.PORT_OFFSET + peer.getPortNumber());
+                    int peerPort = Config.PORT_OFFSET + Integer.parseInt(peer.getPeerID());
+                    Socket socket = new Socket("localhost", peerPort);
                     ObjectOutputStream peerOut = new ObjectOutputStream(socket.getOutputStream()); //stream write to the socket
                     peerOut.flush();
                     ObjectInputStream peerIn = new ObjectInputStream(socket.getInputStream()); //stream read from the socket
 
                     peer.setSocket(socket, peerIn, peerOut);
-                    new PeerHandler(peer, state);
+                    new PeerHandler(peer, state).start();
+                    Log.log(selfInfo.getPeerID() + " successfully connected to peer " + peer.getPeerID(),
+                            selfInfo.getPeerID());
                     break;
                 } catch (IOException e) {
                     Log.log(selfInfo.getPeerID() + " failed to connect to peer " + peer.getPeerID(),
@@ -64,12 +68,15 @@ public class Client {
         }
 
         // Listen for peers
-        int port = Config.PORT_OFFSET + selfInfo.getPortNumber();
+        int port = Config.PORT_OFFSET + Integer.parseInt(selfInfo.getPeerID());
         Log.log("Listening for peers on port " + port, selfInfo.getPeerID());
         while(true) {
             try (ServerSocket listener = new ServerSocket(port)) {
                 new PeerHandler(listener.accept(), state).start();
+                Log.log(selfInfo.getPeerID() + " received new connection attempt ",
+                        selfInfo.getPeerID());
             } catch (IOException e) {
+                Log.log("Connection with peer terminated", selfInfo.getPeerID());
                 throw new RuntimeException(e);
             }
         }
